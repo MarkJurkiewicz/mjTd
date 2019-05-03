b685453c55094a80b0194bb97fb4eca4a0037ec0
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2019-05-03 00:16:29 -0700

    initial fw controllers, models, and views

---
+++ maJ/models/Todo.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "todos".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Todo extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'todos';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
